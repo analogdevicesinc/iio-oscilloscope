d7cad20ff0591500f34dc329ed2909e5ae8e7c86
Dan Nechita <Dan.Nechita@analog.com>
2013-04-25 12:48:10 +0300

    Change the coding style - no functional changes

--- plugins/AD7303.c
+++ plugins/AD7303.c
@@ -1,9 +1,7 @@
 /**
  * Copyright (C) 2012-2013 Analog Devices, Inc.
  *
- * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * Licensed under the GPL-2.
  *
  **/
 #include <stdio.h>
@@ -25,53 +23,49 @@
 #include "../osc_plugin.h"
 #include "../config.h"
 
-#define SINEWAVE	0
-#define SQUAREWAVE	1
-#define TRIANGLE	2
-#define SAWTOOTH	3
+#define SINEWAVE        0
+#define SQUAREWAVE      1
+#define TRIANGLE        2
+#define SAWTOOTH        3
 
-static unsigned int bufferSize;
-static uint8_t *softBufferCh0;
-static int currentSample = 0;
-
+static unsigned int buffer_size;
+static uint8_t *soft_buffer_ch0;
+static int current_sample = 0;
 static int buffer_fd;
-
 static gint fill_buffer_function = 0;
 
 static struct iio_widget tx_widgets[100];
 static struct iio_widget rx_widgets[100];
 static unsigned int num_tx, num_rx;
 
-static GtkWidget *btnSine;
-static GtkWidget *btnSquare;
-static GtkWidget *btnTriangle;
-static GtkWidget *btnSawtooth;
-static GtkWidget *scaleAmpl;
-static GtkWidget *scaleOffset;
-static GtkWidget *scaleFreq;
-static GtkWidget *radioSingleVal;
-static GtkWidget *radioWaveform;
+static GtkWidget *btn_sine;
+static GtkWidget *btn_square;
+static GtkWidget *btn_triangle;
+static GtkWidget *btn_sawtooth;
+static GtkWidget *scale_ampl;
+static GtkWidget *scale_offset;
+static GtkWidget *scale_freq;
+static GtkWidget *radio_single_val;
+static GtkWidget *radio_waveform;
 static GtkWidget *databox;
-static GtkWidget *previewGraph;
+static GtkWidget *preview_graph;
 
 static GdkColor color_background = {
 	.red = 0,
 	.green = 0,
 	.blue = 0,
 };
-
 static GdkColor color_prev_graph = {
 	.red = 0,
 	.green = 65535,
 	.blue = 0,
 };
-
 static GtkDataboxGraph *databox_graph;
 static gfloat *X = NULL;
-static gfloat *floatSoftBuff;
+static gfloat *float_soft_buff;
 
-static gdouble waveAmpl;
-static gdouble waveOffset;
+static gdouble wave_ampl;
+static gdouble wave_offset;
 
 static int buffer_open(unsigned int length)
 {
@@ -125,77 +119,70 @@ static void buffer_close(unsigned int fd)
 	close(fd);
 }
 
-static int FillSoftBuffer(int waveType, uint8_t* softBuffer)
+static int FillSoftBuffer(int waveType, uint8_t *softBuffer)
 {
 	int sampleNr = 0;
 	int rawVal;
 	int intAmpl;
 	int intOffset;
-	
-	intAmpl = waveAmpl  * (256 / 3.3);
-	intOffset = waveOffset * (256 / 3.3);
-	  
-	switch(waveType){
+
+	intAmpl = wave_ampl  * (256 / 3.3);
+	intOffset = wave_offset * (256 / 3.3);
+
+	switch (waveType){
 	case SINEWAVE:
-		for(;sampleNr < bufferSize; sampleNr++)
-		{
-			
-			rawVal = (intAmpl/ 2) * sin(2 * sampleNr * G_PI / bufferSize) + intOffset;
-			if(rawVal < 0)
+		for (; sampleNr < buffer_size; sampleNr++){
+			rawVal = (intAmpl/ 2) * sin(2 * sampleNr * G_PI / buffer_size) + intOffset;
+			if (rawVal < 0)
 				rawVal = 0;
-			else if(rawVal > 255)
+			else if (rawVal > 255)
 				rawVal = 255;
 			softBuffer[sampleNr] = rawVal;
 		}
 		break;
 	case SQUAREWAVE:
-		for(;sampleNr < bufferSize / 2; sampleNr++)
-		{
+		for (; sampleNr < buffer_size / 2; sampleNr++){
 			rawVal = intOffset - (intAmpl/ 2);
-			if(rawVal < 0)
+			if (rawVal < 0)
 				rawVal = 0;
-			else if(rawVal > 255)
+			else if (rawVal > 255)
 				rawVal = 255;
 			softBuffer[sampleNr] = rawVal;
-			
+
 		}
-		for(;sampleNr < bufferSize; sampleNr++)
-		{
+		for (; sampleNr < buffer_size; sampleNr++){
 			rawVal = intOffset + (intAmpl/ 2);
-			if(rawVal < 0)
+			if (rawVal < 0)
 				rawVal = 0;
-			else if(rawVal > 255)
+			else if (rawVal > 255)
 				rawVal = 255;
 			softBuffer[sampleNr] = rawVal;
 		}
 		break;
 	case TRIANGLE:
-		for(;sampleNr < bufferSize / 2; sampleNr++)
-		{
-			rawVal = sampleNr * intAmpl / (bufferSize / 2) + (intOffset - intAmpl / 2 );
-			if(rawVal < 0)
+		for (; sampleNr < buffer_size / 2; sampleNr++){
+			rawVal = sampleNr * intAmpl / (buffer_size / 2) + (intOffset - intAmpl / 2 );
+			if (rawVal < 0)
 				rawVal = 0;
-			else if(rawVal > 255)
+			else if (rawVal > 255)
 				rawVal = 255;
 			softBuffer[sampleNr] = rawVal;
 		}
-		for(sampleNr = 0; sampleNr < (bufferSize +1) / 2; sampleNr++)
-		{
-			rawVal = intAmpl - sampleNr * intAmpl / (bufferSize / 2) + (intOffset - intAmpl / 2 );
-			if(rawVal < 0)
+		for (sampleNr = 0; sampleNr < (buffer_size +1) / 2; sampleNr++){
+			rawVal = intAmpl - sampleNr * intAmpl / (buffer_size / 2) + (intOffset - intAmpl / 2 );
+			if (rawVal < 0)
 				rawVal = 0;
-			else if(rawVal > 255)
+			else if (rawVal > 255)
 				rawVal = 255;
-			softBuffer[sampleNr + bufferSize / 2] = rawVal;
+			softBuffer[sampleNr + buffer_size / 2] = rawVal;
 		}
 		break;
 	case SAWTOOTH:
-		for(;sampleNr < bufferSize; sampleNr++)
-		{
-			rawVal = sampleNr * intAmpl / bufferSize + (intOffset - intAmpl / 2 );
-			if(rawVal < 0)
+		for (; sampleNr < buffer_size; sampleNr++){
+			rawVal = sampleNr * intAmpl / buffer_size + (intOffset - intAmpl / 2 );
+			if (rawVal < 0)
 				rawVal = 0;
-			else if(rawVal > 255)
+			else if (rawVal > 255)
 				rawVal = 255;
 			softBuffer[sampleNr] = rawVal;
 		}
@@ -213,55 +200,50 @@ static void generateWavePeriod(void)
 	int triggerFreq = 100;
 	double waveFreq;
 	int i;
-	
+
 	set_dev_paths("hrtimer-1");
 	read_devattr_int("frequency", &triggerFreq);
-	
+
 	/* Set the maximum frequency that user cand select to 10% of the input generator frequency. */
 	if (triggerFreq >= 10)
-		gtk_range_get_adjustment((GtkRange *)scaleFreq)->upper = triggerFreq / 10;
-		
-	waveAmpl = gtk_range_get_adjustment((GtkRange *)scaleAmpl)->value;
-	waveOffset = gtk_range_get_adjustment((GtkRange *)scaleOffset)->value;
-	waveFreq = gtk_range_get_adjustment((GtkRange *)scaleFreq)->value;
-	bufferSize = (unsigned int)round(triggerFreq / waveFreq);
-	if (bufferSize < 2)
-		bufferSize = 2;
-	else if(bufferSize > 10000)
-		bufferSize = 10000;
-	currentSample = 0;
+		gtk_range_get_adjustment((GtkRange *)scale_freq)->upper = triggerFreq / 10;
 
-	softBufferCh0 = g_renew(uint8_t, softBufferCh0, bufferSize);
-	
-	gtk_range_get_adjustment((GtkRange *)scaleFreq)->value = (double)triggerFreq / bufferSize;
-	gtk_widget_queue_draw(scaleFreq);
-	
-	if(gtk_toggle_tool_button_get_active(GTK_TOGGLE_TOOL_BUTTON(btnSine)))
-	{
+	wave_ampl = gtk_range_get_adjustment((GtkRange *)scale_ampl)->value;
+	wave_offset = gtk_range_get_adjustment((GtkRange *)scale_offset)->value;
+	waveFreq = gtk_range_get_adjustment((GtkRange *)scale_freq)->value;
+	buffer_size = (unsigned int)round(triggerFreq / waveFreq);
+	if (buffer_size < 2)
+		buffer_size = 2;
+	else if (buffer_size > 10000)
+		buffer_size = 10000;
+	current_sample = 0;
+
+	soft_buffer_ch0 = g_renew(uint8_t, soft_buffer_ch0, buffer_size);
+
+	gtk_range_get_adjustment((GtkRange *)scale_freq)->value =
+						(double)triggerFreq / buffer_size;
+	gtk_widget_queue_draw(scale_freq);
+
+	if(gtk_toggle_tool_button_get_active(GTK_TOGGLE_TOOL_BUTTON(btn_sine)))
 		waveType = SINEWAVE;
-	}
-	else if(gtk_toggle_tool_button_get_active(GTK_TOGGLE_TOOL_BUTTON(btnSquare)))
-	{
+	else if (gtk_toggle_tool_button_get_active(GTK_TOGGLE_TOOL_BUTTON(btn_square)))
 		waveType = SQUAREWAVE;
-	}
-	else if(gtk_toggle_tool_button_get_active(GTK_TOGGLE_TOOL_BUTTON(btnTriangle)))
-	{
+	else if (gtk_toggle_tool_button_get_active(GTK_TOGGLE_TOOL_BUTTON(btn_triangle)))
 		waveType = TRIANGLE;
-	}
-	else if(gtk_toggle_tool_button_get_active(GTK_TOGGLE_TOOL_BUTTON(btnSawtooth)))
-	{
+	else if (gtk_toggle_tool_button_get_active(GTK_TOGGLE_TOOL_BUTTON(btn_sawtooth)))
 		waveType = SAWTOOTH;
-	}
-	FillSoftBuffer(waveType, softBufferCh0);
-	
-	floatSoftBuff = g_renew(gfloat, floatSoftBuff,  2 * bufferSize);
-	X = g_renew(gfloat, X, 2 * bufferSize);
-	for (i = 0; i < 2 * bufferSize; i++)
+	FillSoftBuffer(waveType, soft_buffer_ch0);
+
+	/* Also generate a preview of the output signal. */
+	float_soft_buff = g_renew(gfloat, float_soft_buff,  2 * buffer_size);
+	X = g_renew(gfloat, X, 2 * buffer_size);
+	for (i = 0; i < 2 * buffer_size; i++)
 		X[i] = i;
-	for (i = 0; i < 2 * bufferSize; i++)
-		floatSoftBuff[i] = softBufferCh0[i % bufferSize] * 3.3 / 256;
+	for (i = 0; i < 2 * buffer_size; i++)
+		float_soft_buff[i] = soft_buffer_ch0[i % buffer_size] * 3.3 / 256;
 	gtk_databox_graph_remove_all(GTK_DATABOX(databox));
-	databox_graph = gtk_databox_lines_new((2 * bufferSize), X, floatSoftBuff, &color_prev_graph, 2);
+	databox_graph = gtk_databox_lines_new((2 * buffer_size), X, float_soft_buff,
+							&color_prev_graph, 2);
 	gtk_databox_graph_add(GTK_DATABOX(databox), databox_graph);
 	gtk_databox_set_total_limits(GTK_DATABOX(databox), 0, (i - 1), 3.5, 0);
 }
@@ -270,23 +252,18 @@ static gboolean fillBuffer(void)
 {
 	int samplesToSend;
 	int ret;
-	
-	samplesToSend = bufferSize - currentSample;
-	ret = write(buffer_fd, softBufferCh0 + currentSample, samplesToSend);
-	if(ret < 0)
-	{
+
+	samplesToSend = buffer_size - current_sample;
+	ret = write(buffer_fd, soft_buffer_ch0 + current_sample, samplesToSend);
+	if (ret < 0)
 		printf("Error occured while writing to buffer: %d\n", errno);
-	}
-	else
-	{
-		currentSample += ret;
-		if(currentSample >= bufferSize)
-		{
-			currentSample = 0;
-		}
+	else {
+		current_sample += ret;
+		if (current_sample >= buffer_size)
+			current_sample = 0;
 	}
 	usleep(10000);
-	
+
 	return TRUE;
 }
 
@@ -307,54 +284,54 @@ static void rx_update_values(void)
 	rx_update_labels();
 }
 
-static void sine_button_toggled(GtkToggleToolButton *btn, 
+static void sine_button_toggled(GtkToggleToolButton *btn,
 							gpointer user_data)
 {
 	if (gtk_toggle_tool_button_get_active(GTK_TOGGLE_TOOL_BUTTON(btn))){
-		gtk_toggle_tool_button_set_active(GTK_TOGGLE_TOOL_BUTTON(btnSquare),
+		gtk_toggle_tool_button_set_active(GTK_TOGGLE_TOOL_BUTTON(btn_square),
 									FALSE);
-		gtk_toggle_tool_button_set_active(GTK_TOGGLE_TOOL_BUTTON(btnTriangle),
+		gtk_toggle_tool_button_set_active(GTK_TOGGLE_TOOL_BUTTON(btn_triangle),
 									FALSE);
-		gtk_toggle_tool_button_set_active(GTK_TOGGLE_TOOL_BUTTON(btnSawtooth),
-									FALSE);
-		generateWavePeriod();
-	}
-}
-static void square_button_toggled(GtkToggleToolButton *togglebutton, 
-							gpointer user_data)
-{
-	if (gtk_toggle_tool_button_get_active(GTK_TOGGLE_TOOL_BUTTON(togglebutton))){
-		gtk_toggle_tool_button_set_active(GTK_TOGGLE_TOOL_BUTTON(btnSine),
-									FALSE);
-		gtk_toggle_tool_button_set_active(GTK_TOGGLE_TOOL_BUTTON(btnTriangle),
-									FALSE);
-		gtk_toggle_tool_button_set_active(GTK_TOGGLE_TOOL_BUTTON(btnSawtooth),
+		gtk_toggle_tool_button_set_active(GTK_TOGGLE_TOOL_BUTTON(btn_sawtooth),
 									FALSE);
 		generateWavePeriod();
 	}
 }
-static void triangle_button_toggled(GtkToggleToolButton *togglebutton, 
+static void square_button_toggled(GtkToggleToolButton *togglebutton,
 							gpointer user_data)
 {
 	if (gtk_toggle_tool_button_get_active(GTK_TOGGLE_TOOL_BUTTON(togglebutton))){
-		gtk_toggle_tool_button_set_active(GTK_TOGGLE_TOOL_BUTTON(btnSine),
+		gtk_toggle_tool_button_set_active(GTK_TOGGLE_TOOL_BUTTON(btn_sine),
 									FALSE);
-		gtk_toggle_tool_button_set_active(GTK_TOGGLE_TOOL_BUTTON(btnSquare),
+		gtk_toggle_tool_button_set_active(GTK_TOGGLE_TOOL_BUTTON(btn_triangle),
 									FALSE);
-		gtk_toggle_tool_button_set_active(GTK_TOGGLE_TOOL_BUTTON(btnSawtooth),
+		gtk_toggle_tool_button_set_active(GTK_TOGGLE_TOOL_BUTTON(btn_sawtooth),
 									FALSE);
 		generateWavePeriod();
 	}
 }
-static void sawtooth_button_toggled(GtkToggleToolButton *togglebutton, 
+static void triangle_button_toggled(GtkToggleToolButton *togglebutton,
 							gpointer user_data)
 {
 	if (gtk_toggle_tool_button_get_active(GTK_TOGGLE_TOOL_BUTTON(togglebutton))){
-		gtk_toggle_tool_button_set_active(GTK_TOGGLE_TOOL_BUTTON(btnSine),
+		gtk_toggle_tool_button_set_active(GTK_TOGGLE_TOOL_BUTTON(btn_sine),
 									FALSE);
-		gtk_toggle_tool_button_set_active(GTK_TOGGLE_TOOL_BUTTON(btnTriangle),
+		gtk_toggle_tool_button_set_active(GTK_TOGGLE_TOOL_BUTTON(btn_square),
 									FALSE);
-		gtk_toggle_tool_button_set_active(GTK_TOGGLE_TOOL_BUTTON(btnSquare),
+		gtk_toggle_tool_button_set_active(GTK_TOGGLE_TOOL_BUTTON(btn_sawtooth),
+									FALSE);
+		generateWavePeriod();
+	}
+}
+static void sawtooth_button_toggled(GtkToggleToolButton *togglebutton,
+							gpointer user_data)
+{
+	if (gtk_toggle_tool_button_get_active(GTK_TOGGLE_TOOL_BUTTON(togglebutton))){
+		gtk_toggle_tool_button_set_active(GTK_TOGGLE_TOOL_BUTTON(btn_sine),
+									FALSE);
+		gtk_toggle_tool_button_set_active(GTK_TOGGLE_TOOL_BUTTON(btn_triangle),
+									FALSE);
+		gtk_toggle_tool_button_set_active(GTK_TOGGLE_TOOL_BUTTON(btn_square),
 									FALSE);
 		generateWavePeriod();
 	}
@@ -373,16 +350,13 @@ static void save_button_clicked(GtkButton *btn, gpointer data)
 		buffer_fd = -1;
 	}
 
-	if(gtk_toggle_button_get_active((GtkToggleButton *)radioSingleVal))
-	{
+	if (gtk_toggle_button_get_active((GtkToggleButton *)radio_single_val)){
 		iio_save_widgets(tx_widgets, num_tx);
 		iio_save_widgets(rx_widgets, num_rx);
 		rx_update_labels();
-	}
-	else if(gtk_toggle_button_get_active((GtkToggleButton *)radioWaveform))
-	{
+	} else if (gtk_toggle_button_get_active((GtkToggleButton *)radio_waveform)){
 		generateWavePeriod();
-		buffer_fd = buffer_open(bufferSize * 10);
+		buffer_fd = buffer_open(buffer_size * 10);
 		startWaveGeneration();
 	}
 }
@@ -399,17 +373,17 @@ static int AD7303_init(GtkWidget *notebook)
 		gtk_builder_add_from_file(builder, OSC_GLADE_FILE_PATH "AD7303.glade", NULL);
 
 	AD7303_panel = GTK_WIDGET(gtk_builder_get_object(builder, "tablePanelAD7303"));
-	btnSine = GTK_WIDGET(gtk_builder_get_object(builder, "togBtnSine"));
-	btnSquare = GTK_WIDGET(gtk_builder_get_object(builder, "togBtnSquare"));
-	btnTriangle = GTK_WIDGET(gtk_builder_get_object(builder, "togBtnTriangle"));
-	btnSawtooth = GTK_WIDGET(gtk_builder_get_object(builder, "togBtnSawth"));
-	scaleAmpl = GTK_WIDGET(gtk_builder_get_object(builder, "vscaleAmpl"));
-	scaleOffset = GTK_WIDGET(gtk_builder_get_object(builder, "vscaleOff"));
-	scaleFreq = GTK_WIDGET(gtk_builder_get_object(builder, "vscaleFreq"));
-	radioSingleVal = GTK_WIDGET(gtk_builder_get_object(builder, "radioSingleVal"));
-	radioWaveform = GTK_WIDGET(gtk_builder_get_object(builder, "radioWaveform"));
-	previewGraph = GTK_WIDGET(gtk_builder_get_object(builder, "vboxDatabox"));
-	
+	btn_sine = GTK_WIDGET(gtk_builder_get_object(builder, "togBtnSine"));
+	btn_square = GTK_WIDGET(gtk_builder_get_object(builder, "togBtnSquare"));
+	btn_triangle = GTK_WIDGET(gtk_builder_get_object(builder, "togBtnTriangle"));
+	btn_sawtooth = GTK_WIDGET(gtk_builder_get_object(builder, "togBtnSawth"));
+	scale_ampl = GTK_WIDGET(gtk_builder_get_object(builder, "vscaleAmpl"));
+	scale_offset = GTK_WIDGET(gtk_builder_get_object(builder, "vscaleOff"));
+	scale_freq = GTK_WIDGET(gtk_builder_get_object(builder, "vscaleFreq"));
+	radio_single_val = GTK_WIDGET(gtk_builder_get_object(builder, "radioSingleVal"));
+	radio_waveform = GTK_WIDGET(gtk_builder_get_object(builder, "radioWaveform"));
+	preview_graph = GTK_WIDGET(gtk_builder_get_object(builder, "vboxDatabox"));
+
 	/* Bind the IIO device files to the GUI widgets */
 	iio_spin_button_init_from_builder(&tx_widgets[num_tx++],
 			"ad7303", "out_voltage0_raw",
@@ -424,32 +398,31 @@ static int AD7303_init(GtkWidget *notebook)
 			"AD7303", "out_voltage1_powerdown",
 			builder, "checkbuttonPwrDwn1", 0);
 
-	g_signal_connect(btnSine, "toggled", G_CALLBACK(sine_button_toggled), 
+	g_signal_connect(btn_sine, "toggled", G_CALLBACK(sine_button_toggled),
 									NULL);
-	g_signal_connect(btnSquare, "toggled", G_CALLBACK(square_button_toggled), 
+	g_signal_connect(btn_square, "toggled", G_CALLBACK(square_button_toggled),
 									NULL);
-	g_signal_connect(btnTriangle, "toggled", 
+	g_signal_connect(btn_triangle, "toggled",
 				G_CALLBACK(triangle_button_toggled), NULL);
-	g_signal_connect(btnSawtooth, "toggled", 
+	g_signal_connect(btn_sawtooth, "toggled",
 				G_CALLBACK(sawtooth_button_toggled), NULL);
-	g_signal_connect(scaleAmpl, "value-changed",
+	g_signal_connect(scale_ampl, "value-changed",
 				G_CALLBACK(wave_param_changed), NULL);
-	g_signal_connect(scaleOffset, "value-changed",
+	g_signal_connect(scale_offset, "value-changed",
 				G_CALLBACK(wave_param_changed), NULL);
-	g_signal_connect(scaleFreq, "value-changed",
+	g_signal_connect(scale_freq, "value-changed",
 				G_CALLBACK(wave_param_changed), NULL);
 	g_builder_connect_signal(builder, "buttonSave", "clicked",
 					G_CALLBACK(save_button_clicked), NULL);
-
 	/* Create a GtkDatabox widget */
-	gtk_databox_create_box_with_scrollbars_and_rulers(&databox, &table, 
+	gtk_databox_create_box_with_scrollbars_and_rulers(&databox, &table,
 						TRUE, TRUE, TRUE, TRUE);
-	gtk_box_pack_start(GTK_BOX(previewGraph), table, TRUE, TRUE, 0);
+	gtk_box_pack_start(GTK_BOX(preview_graph), table, TRUE, TRUE, 0);
 	gtk_widget_modify_bg(databox, GTK_STATE_NORMAL, &color_background);
 	gtk_widget_set_size_request(table, 450, 300);
 
 	gtk_widget_show_all(AD7303_panel);
-	
+
 	tx_update_values();
 	rx_update_values();
 
