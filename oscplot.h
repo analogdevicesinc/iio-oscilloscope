2c4e2b7f204b55d43a9cf664c81be896a51a703b
dNechita <Dan.Nechita@analog.com>
2013-09-13 18:02:13 +0300

    oscplot.h Correct some mistakes.

--- oscplot.h
+++ oscplot.h
@@ -16,8 +16,8 @@ G_BEGIN_DECLS
 #define OSC_PLOT_TYPE              (osc_plot_get_type())
 #define OSC_PLOT(obj)              (G_TYPE_CHECK_INSTANCE_CAST((obj), OSC_PLOT_TYPE, OscPlot))
 #define OSC_PLOT_CLASS(klass)      (G_TYPE_CHECK_CLASS_CAST((klass), OSC_PLOT_TYPE, OscPlotClass))
-#define IS_OSC_PLOT(obj)           (G_TYPE_CHECK_INSTANCE_CAST((obj), OSC_PLOT_TYPE))
-#define IS_OSC_PLOT_CLASS(klass)   (G_TYPE_CHECK_CLASS_CAST((klass), OSC_PLOT_TYPE))
+#define IS_OSC_PLOT(obj)           (G_TYPE_CHECK_INSTANCE_TYPE((obj), OSC_PLOT_TYPE))
+#define IS_OSC_PLOT_CLASS(klass)   (G_TYPE_CHECK_CLASS_TYPE((klass), OSC_PLOT_TYPE))
 
 typedef struct _OscPlot            OscPlot;
 typedef struct _OscPlotPrivate     OscPlotPrivate;
@@ -26,10 +26,7 @@ typedef struct _OscPlotClass       OscPlotClass;
 struct _OscPlot
 {
 	GtkBin bin;
-	
 	OscPlotPrivate *priv;
-
-	gfloat *x_axis_source;
 };
 
 struct _OscPlotClass
