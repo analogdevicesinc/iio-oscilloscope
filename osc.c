05d30a6a632455ee884ec3cc49ce4c42f68ccd77
dNechita <Dan.Nechita@analog.com>
2014-01-15 11:59:00 +0000

    PROFILE: When failing - bring up a window, so people know why things are quiting

--- osc.c
+++ osc.c
@@ -1531,8 +1531,10 @@ static int load_default_profile (char *filename)
 			if (linecount == ret) {
 				tmp[strlen(tmp) - 1] = 0;
 				if (strcmp(tmp, "quit = 1")) {
-					printf("Error parsing profile '%s'\n\tline %i : '%s'\n",
-							buf, ret, tmp);
+					create_blocking_popup(GTK_MESSAGE_ERROR, GTK_BUTTONS_CLOSE,
+						"INI parsing / test failure",
+						"Error parsing file '%s'\n\tline %i : '%s'\n",
+						buf, ret, tmp);
 				} else
 					ret = -ENOTTY;
 				break;
