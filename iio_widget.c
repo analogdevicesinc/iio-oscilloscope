f5b7836215e61eb7d41af7e64591e7ad3bbfc1ca
Dnechita <Dan.Nechita@analog.com>
2014-03-12 13:45:12 +0000

    iio_widget lib: Add implementation for spinbuttons with progress bar.     - Spinbuttons with progress bar are used in situations where you don't want the value of the button to be written right away, but instead it waits for a short period of time.     - This is helpful when you deal with attributes that can take only specific values and automatically they change user's input value to the nearest value that the attribute can take.     - Whithout it, you will not be able to spin up or down the spinbutton

--- iio_widget.c
+++ iio_widget.c
@@ -309,3 +309,158 @@ void iio_toggle_button_init_from_builder(struct iio_widget *widget,
 	iio_toggle_button_init(widget, device_name, attr_name,
 		GTK_WIDGET(gtk_builder_get_object(builder, widget_name)), invert);
 }
+
+/*
+ * struct progress_data - Information about the progress spinbutton
+ *
+ * @is_progress_spin_button: Used for progress spinbutton identification
+ * @progress: Progress status. Range between 0.0 and 1.0
+ * @timeoutID: Handler ID of callback that increases progress step by step
+ * @value_changed_hid: Handler ID of callback for a "value-changed" event of the
+ *                     progress spinbutton
+ * @on_complete: Function to be called when progress reaches 1.0.
+ */
+struct progress_data {
+	gboolean is_progress_spin_button;
+	gfloat progress;
+	gint timeoutID;
+	gint value_changed_hid;
+	void (*on_complete)(void);
+};
+
+/*
+ * Gets called periodically to increase the progress with one step.
+ * When progress is complete saves the spinbutton value to file, clears the
+ * progress and stops the function to be called periodically.
+ */
+static gboolean spin_button_progress_step(struct iio_widget *iio_w)
+{
+	struct progress_data *pdata = iio_w->priv_progress;
+	void (*on_complete_cb)(void) = pdata->on_complete;
+
+	if (pdata->progress < 1.0) {
+		pdata->progress += 0.095;
+		gtk_entry_set_progress_fraction(GTK_ENTRY(iio_w->widget), pdata->progress);
+
+		return TRUE;
+	} else {
+		pdata->progress = 0.0;
+		gtk_entry_set_progress_fraction(GTK_ENTRY(iio_w->widget), pdata->progress);
+		iio_widget_save(iio_w);
+		if (pdata->on_complete != NULL)
+			on_complete_cb();
+		pdata->timeoutID = -1;
+
+		return FALSE;
+	}
+}
+
+/*
+ * When a "value-changed" event of the spinbutton occurs the progress bar of the
+ * spinbutton starts to increase until reaches the complete state. If another
+ * event occurs while the progress is not finished, the progress will be reset.
+ */
+
+static void delayed_spin_button_update_cb(GtkSpinButton *spinbutton,
+	struct iio_widget *iio_w)
+{
+	struct progress_data *pdata = iio_w->priv_progress;
+
+	if (pdata->timeoutID != - 1)
+		pdata->progress = 0.0;
+	else
+		pdata->timeoutID = g_timeout_add(90, (GSourceFunc)spin_button_progress_step, iio_w);
+}
+
+/*
+ * Customises a iio_widget that contains a GtkSpinButton widget to be able to
+ * work as a progress spinbutton.
+ */
+void iio_spin_button_add_progress(struct iio_widget *iio_w)
+{
+	struct progress_data *pdata;
+
+	if (GTK_IS_SPIN_BUTTON(iio_w->widget) == FALSE) {
+		printf("The widget connected to the attribute: %s of device: %s is not a GtkSpinButton\n",
+			iio_w->attr_name, iio_w->device_name);
+		return;
+	}
+
+	pdata = malloc(sizeof(struct progress_data));
+	pdata->is_progress_spin_button = TRUE;
+	pdata->progress = 0.0;
+	pdata->timeoutID = -1;
+	pdata->value_changed_hid = -1;
+	pdata->on_complete = NULL;
+	iio_w->priv_progress = pdata;
+}
+
+/*
+ * In order for the progress spinbutton to work, this function must be called
+ * to connect the required callback to the widget.
+ */
+void iio_spin_button_progress_activate(struct iio_widget *iio_w)
+{
+	struct progress_data *pdata = iio_w->priv_progress;
+
+	if (GTK_IS_SPIN_BUTTON(iio_w->widget) == FALSE) {
+		printf("The widget connected to the attribute: %s of device: %s is not a GtkSpinButton\n",
+			iio_w->attr_name, iio_w->device_name);
+		return;
+	}
+
+	pdata->value_changed_hid = g_signal_connect(G_OBJECT(iio_w->widget),
+		"value-changed", G_CALLBACK(delayed_spin_button_update_cb), iio_w);
+}
+
+
+/*
+ * Set the a user function to be called when the progress is completed.
+ */
+void iio_spin_button_set_on_complete_function(struct iio_widget *iio_w,
+	void(*on_complete)(void))
+{
+	struct progress_data *pdata = iio_w->priv_progress;
+
+	if (GTK_IS_SPIN_BUTTON(iio_w->widget) == FALSE) {
+		printf("The widget connected to the attribute: %s of device: %s is not a GtkSpinButton\n",
+			iio_w->attr_name, iio_w->device_name);
+		return;
+	}
+
+	pdata->on_complete = on_complete;
+}
+
+/*
+ * Disconnect the callback of the "value-changed" event from the progress
+ * spinbutton.
+ */
+void iio_spin_button_progress_deactivate(struct iio_widget *iio_w)
+{
+	struct progress_data *pdata = iio_w->priv_progress;
+
+	if (GTK_IS_SPIN_BUTTON(iio_w->widget) == FALSE) {
+		printf("The widget connected to the attribute: %s of device: %s is not a GtkSpinButton\n",
+			iio_w->attr_name, iio_w->device_name);
+		return;
+	}
+
+	g_signal_handler_disconnect(iio_w, pdata->value_changed_hid);
+}
+
+/*
+ * Remove all implementation made to convert a iio_widget to a progress
+ * spinbutton.
+ */
+void iio_spin_button_remove_progress(struct iio_widget *iio_w)
+{
+	if (GTK_IS_SPIN_BUTTON(iio_w->widget) == FALSE) {
+		printf("The widget connected to the attribute: %s of device: %s is not a GtkSpinButton\n",
+			iio_w->attr_name, iio_w->device_name);
+		return;
+	}
+
+	iio_spin_button_progress_deactivate(iio_w);
+	if (iio_w->priv_progress)
+		free(iio_w->priv_progress);
+}
