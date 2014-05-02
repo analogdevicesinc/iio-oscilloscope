491a2827ac5572a1a896962cab43f0fcebb34130
Robin Getz <robin.getz@analog.com>
2014-05-02 12:49:55 +0000

    DATAFILE: Whitespace changes only - fix indenting
    Signed-off-by: Robin Getz <Robin.Getz@analog.com>

--- plugins/datafile_in.h
+++ plugins/datafile_in.h
@@ -22,89 +22,94 @@ static int analyse_wavefile(const char *file_name, char **buf, int *count, int t
 		return -3;
 
 	if (fgets(line, 80, infile) != NULL) {
-	if (strncmp(line, "TEXT", 4) == 0) {
-		/* Unscaled samples need to be in the range +- 2047 */
-		if (strncmp(line, "TEXTU", 5) == 0)
-			scale = 16.0; /* scale up to 16-bit */
-		ret = sscanf(line, "TEXT%*c REPEAT %d", &rep);
-		if (ret != 1) {
-			rep = 1;
-		}
-		size = 0;
-		while (fgets(line, 80, infile)) {
-			ret = sscanf(line, "%lf%*[, \t]%lf%*[, \t]%lf%*[, \t]%lf",
-				     &val[0], &val[1], &val[2], &val[3]);
+		if (strncmp(line, "TEXT", 4) == 0) {
+			/* Unscaled samples need to be in the range +- 2047 */
+			if (strncmp(line, "TEXTU", 5) == 0)
+				scale = 16.0;	/* scale up to 16-bit */
+			ret = sscanf(line, "TEXT%*c REPEAT %d", &rep);
+			if (ret != 1) {
+				rep = 1;
+			}
+			size = 0;
+			while (fgets(line, 80, infile)) {
+				ret = sscanf(line, "%lf%*[, \t]%lf%*[, \t]%lf%*[, \t]%lf",
+						&val[0], &val[1], &val[2], &val[3]);
 
-			if (!(ret == 4 || ret == 2)) {
-				fclose(infile);
-				return -2;
+				if (!(ret == 4 || ret == 2)) {
+					fclose(infile);
+					return -2;
+				}
+
+				for (i = 0; i < ret; i++)
+					if (fabs(val[i]) > max)
+						max = fabs(val[i]);
+
+				size += ((tx == 2) ? 8 : 4);
+
+
 			}
 
-			for (i = 0; i < ret; i++)
-				if (fabs(val[i]) > max)
-					max = fabs(val[i]);
+			size *= rep;
+			if (scale == 0.0)
+				scale = 32767.0 / max;
 
-			size += ((tx == 2) ? 8 : 4);
+			if (max > 32767.0)
+				fprintf(stderr, "ERROR: DAC Waveform Samples > +/- 2047.0\n");
 
+			*buf = malloc(size);
+			if (*buf == NULL)
+				return 0;
 
-		}
+			unsigned long long *sample = *((unsigned long long **) buf);
+			unsigned int *sample_32 = *((unsigned int **) buf);
 
-	size *= rep;
-	if (scale == 0.0)
-		scale = 32767.0 / max;
+			rewind(infile);
 
-	if (max > 32767.0)
-		fprintf(stderr, "ERROR: DAC Waveform Samples > +/- 2047.0\n");
+			if (fgets(line, 80, infile) != NULL) {
+				if (strncmp(line, "TEXT", 4) == 0) {
+					size = 0;
+					i = 0;
+					while (fgets(line, 80, infile)) {
 
-	*buf = malloc(size);
-	if (*buf == NULL)
-		return 0;
+						ret = sscanf(line, "%lf%*[, \t]%lf%*[, \t]%lf%*[, \t]%lf",
+								&i1, &q1, &i2, &q2);
+						for (j = 0; j < rep; j++) {
+							if (ret == 4 && tx == 2) {
+								sample[i++] = ((unsigned long long) convert(scale, q2) << 48) +
+								    ((unsigned long long) convert(scale, i2) << 32) +
+								    (convert(scale, q1) << 16) +
+								    (convert(scale, i1) << 0);
 
-	unsigned long long *sample = *((unsigned long long **)buf);
-	unsigned int *sample_32 = *((unsigned int **)buf);
+								size += 8;
+							}
+							if (ret == 2 && tx == 2) {
+								sample[i++] = ((unsigned long long) convert(scale, q1) << 48) +
+								    ((unsigned long long) convert(scale, i1) << 32) +
+								    (convert(scale, q1) << 16) +
+								    (convert(scale, i1) << 0);
 
-	rewind(infile);
+								size += 8;
+							}
+							if (tx == 1) {
+								sample_32[i++] = (convert(scale, q1) << 16) +
+									(convert(scale, i1) << 0);
 
-	if (fgets(line, 80, infile) != NULL) {
-		if (strncmp(line, "TEXT", 4) == 0) {
-			size = 0;
-			i = 0;
-			while (fgets(line, 80, infile)) {
-
-				ret = sscanf(line, "%lf%*[, \t]%lf%*[, \t]%lf%*[, \t]%lf",
-					     &i1, &q1, &i2, &q2);
-				for (j = 0; j < rep; j++) {
-					if (ret == 4 && tx == 2) {
-						sample[i++] = ((unsigned long long)convert(scale, q2) << 48) +
-							((unsigned long long)convert(scale, i2) << 32) +
-							(convert(scale, q1) << 16) +
-							(convert(scale, i1) << 0);
-
-						size += 8;
-					}
-					if (ret == 2 && tx == 2) {
-						sample[i++] = ((unsigned long long)convert(scale, q1) << 48) +
-							((unsigned long long)convert(scale, i1) << 32) +
-							(convert(scale, q1) << 16) +
-							(convert(scale, i1) << 0);
-
-						size += 8;
-					}
-					if (tx == 1) {
-						sample_32[i++] = (convert(scale, q1) << 16) +
-							(convert(scale, i1) << 0);
-
-						size += 4;
+								size += 4;
+							}
+						}
 					}
 				}
 			}
+
+			fclose(infile);
+			*count = size;
+
+		} else {
+			/* Is it a MATLAB file?
+			 * http://na-wiki.csc.kth.se/mediawiki/index.php/MatIO
+			 */
 		}
-	}
-
-	fclose(infile);
-	*count = size;
-
-	}} else {
+	} else {
 		fclose(infile);
 		*buf = NULL;
 		return -1;
@@ -112,4 +117,3 @@ static int analyse_wavefile(const char *file_name, char **buf, int *count, int t
 
 	return 0;
 }
-
