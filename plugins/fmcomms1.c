f6b8a3413bc1b6009cdadf2a783aeae8952ef4b9
Paul Cercueil <paul.cercueil@analog.com>
2014-06-04 14:18:47 +0200

    plugins/fmcomms1: Save/restore plugin-specific data to/from INI
    Signed-off-by: Paul Cercueil <paul.cercueil@analog.com>

--- plugins/fmcomms1.c
+++ plugins/fmcomms1.c
@@ -33,8 +33,6 @@
 
 #define THIS_DRIVER "FMComms1"
 
-#define SYNC_RELOAD "SYNC_RELOAD"
-
 #define ARRAY_SIZE(x) (!sizeof(x) ?: sizeof(x) / sizeof((x)[0]))
 
 static const gdouble mhz_scale = 1000000.0;
@@ -118,8 +116,6 @@ static const char *fmcomms1_sr_attribs[] = {
 	"cf-ad9122-core-lpc.out_altvoltage_sampling_frequency",
 	"cf-ad9122-core-lpc.out_altvoltage_interpolation_frequency",
 	"cf-ad9122-core-lpc.out_altvoltage_interpolation_center_shift_frequency",
-	"dds_mode",
-	"dac_buf_filename",
 	"cf-ad9122-core-lpc.out_altvoltage0_1A_frequency",
 	"cf-ad9122-core-lpc.out_altvoltage2_2A_frequency",
 	"cf-ad9122-core-lpc.out_altvoltage1_1B_frequency",
@@ -152,8 +148,6 @@ static const char *fmcomms1_sr_attribs[] = {
 	"adf4351-rx-lpc.out_altvoltage0_powerdown",
 	"ad8366-lpc.out_voltage0_hardwaregain",
 	"ad8366-lpc.out_voltage1_hardwaregain",
-	SYNC_RELOAD,
-	NULL,
 };
 
 static int kill_thread;
@@ -2029,6 +2023,91 @@ static void make_widget_update_signal_based(struct iio_widget *widgets,
 	}
 }
 
+static void update_widgets_from_ini(const char *ini_fn)
+{
+	char *value = read_token_from_ini(ini_fn, THIS_DRIVER, "dds_mode");
+	if (value) {
+		gtk_combo_box_set_active(GTK_COMBO_BOX(dds_mode), atoi(value));
+		free(value);
+	}
+
+	if (gtk_combo_box_get_active(GTK_COMBO_BOX(dds_mode)) == 4) {
+		value = read_token_from_ini(ini_fn, THIS_DRIVER, "dac_buf_filename");
+		if (value) {
+			gtk_file_chooser_set_filename(GTK_FILE_CHOOSER(dac_buffer), value);
+			process_dac_buffer_file(value);
+			free(value);
+		}
+	}
+
+	value = read_token_from_ini(ini_fn, THIS_DRIVER, "calibrate_rx_level");
+	if (value) {
+		cal_rx_level = atof(value);
+		free(value);
+	}
+
+	value = read_token_from_ini(ini_fn, THIS_DRIVER, "cal_clear");
+	if (value) {
+		memset(&cal_eeprom_v1, 0, sizeof(cal_eeprom_v1));
+		free(value);
+	}
+
+	value = read_token_from_ini(ini_fn, THIS_DRIVER, "cal_add");
+	if (value) {
+		cal_entry_add(&cal_eeprom_v1);
+		free(value);
+	}
+
+	value = read_token_from_ini(ini_fn, THIS_DRIVER, "cal_save");
+	if (value) {
+		cal_save_to_eeprom(&cal_eeprom_v1);
+		free(value);
+	}
+
+	value = read_token_from_ini(ini_fn, THIS_DRIVER, "calibrate_rx");
+	if (value) {
+		if (atoi(value) == 1) {
+			GThread *thr;
+			unsigned int i = 0;
+
+			gtk_widget_show(dialogs.calibrate);
+			kill_thread = 0;
+			cal_rx_button_clicked();
+			thr = g_thread_new("Display_thread", (void *) &display_cal, (gpointer *)1);
+			while (i <= 20) {
+				i += kill_thread;
+				gtk_main_iteration();
+			}
+			g_thread_join(thr);
+			cal_rx_flag = false;
+			gtk_widget_hide(dialogs.calibrate);
+		}
+		free(value);
+	}
+
+	value = read_token_from_ini(ini_fn, THIS_DRIVER, "calibrate_tx");
+	if (value) {
+		if (atoi(value) == 1) {
+			GThread *thr, *thid;
+			unsigned int i = 0;
+
+			scpi_connect_functions();
+			gtk_widget_show(dialogs.calibrate);
+			kill_thread = 0;
+			thid = cal_tx_button_clicked();
+			thr = g_thread_new("Display_thread", (void *) &display_cal, (gpointer *)1);
+			while (i <= 20) {
+				i += kill_thread;
+				gtk_main_iteration();
+			}
+			g_thread_join(thid);
+			g_thread_join(thr);
+			gtk_widget_hide(dialogs.calibrate);
+		}
+		free(value);
+	}
+}
+
 static GtkWidget * fmcomms1_init(GtkWidget *notebook, const char *ini_fn)
 {
 	GtkBuilder *builder;
@@ -2180,6 +2259,8 @@ static GtkWidget * fmcomms1_init(GtkWidget *notebook, const char *ini_fn)
 	dac_data_clock = GTK_WIDGET(gtk_builder_get_object(builder, "dac_data_clock"));
 	dac_interpolation = GTK_WIDGET(gtk_builder_get_object(builder, "dac_interpolation_clock"));
 	dac_shift = GTK_WIDGET(gtk_builder_get_object(builder, "dac_fcenter_shift"));
+
+	update_widgets_from_ini(ini_fn);
 
 	ch0 = iio_device_find_channel(dac, "altvoltage0", true);
 	scale_available = iio_channel_find_attr(ch0, "scale_available");
@@ -2408,87 +2489,15 @@ static GtkWidget * fmcomms1_init(GtkWidget *notebook, const char *ini_fn)
 	return fmcomms1_panel;
 }
 
-static char *handle_item(struct osc_plugin *plugin, const char *attrib,
-			 const char *value)
+static void save_widgets_to_ini(FILE *f)
 {
-	char *buf;
-	GThread *thr = NULL, *thid = NULL;
-	int i = 0;
+	char buf[0x1000];
 
-	if (MATCH_ATTRIB(SYNC_RELOAD)) {
-		if (value) {
-			tx_update_values();
-			rx_update_values();
-		} else {
-			return "1";
-		}
-	} else if (MATCH_ATTRIB("dds_mode")) {
-		if (value) {
-			gtk_combo_box_set_active(GTK_COMBO_BOX(dds_mode), atoi(value));
-		} else {
-			buf = malloc (10);
-			sprintf(buf, "%i", gtk_combo_box_get_active(GTK_COMBO_BOX(dds_mode)));
-			return buf;
-		}
-	} else if (MATCH_ATTRIB("dac_buf_filename") &&
-				gtk_combo_box_get_active(GTK_COMBO_BOX(dds_mode)) == 4) {
-		if (value) {
-			gtk_file_chooser_set_filename(GTK_FILE_CHOOSER(dac_buffer), value);
-			process_dac_buffer_file(value);
-		} else
-			return dac_buf_filename;
-	} else if (MATCH_ATTRIB("calibrate_rx_level")) {
-		if (value)
-			cal_rx_level = atof(value);
-	} else if (MATCH_ATTRIB("cal_clear")) {
-		if (value)
-			memset(&cal_eeprom_v1, 0, sizeof(cal_eeprom_v1));
-	} else if (MATCH_ATTRIB("cal_add")) {
-		if (value)
-			cal_entry_add(&cal_eeprom_v1);
-	} else if (MATCH_ATTRIB("cal_save")) {
-		if (value)
-			cal_save_to_eeprom(&cal_eeprom_v1);
-
-	} else if (MATCH_ATTRIB("calibrate_rx")) {
-		if (value && atoi(value) == 1) {
-			gtk_widget_show(dialogs.calibrate);
-			kill_thread = 0;
-			cal_rx_button_clicked();
-			thr = g_thread_new("Display_thread", (void *) &display_cal, (gpointer *)1);
-			while (i <= 20) {
-				i += kill_thread;
-				gtk_main_iteration();
-			}
-			g_thread_join(thr);
-			cal_rx_flag = false;
-			gtk_widget_hide(dialogs.calibrate);
-		}
-	} else if (MATCH_ATTRIB("calibrate_tx")) {
-		if (value && atoi(value) == 1) {
-			scpi_connect_functions();
-			gtk_widget_show(dialogs.calibrate);
-			kill_thread = 0;
-			thid = cal_tx_button_clicked();
-			thr = g_thread_new("Display_thread", (void *) &display_cal, (gpointer *)1);
-			while (i <= 20) {
-				i += kill_thread;
-				gtk_main_iteration();
-			}
-			g_thread_join(thid);
-			g_thread_join(thr);
-			gtk_widget_hide(dialogs.calibrate);
-		}
-	} else {
-		if (value) {
-			printf("Unhandled tokens in ini file,\n"
-				"\tSection %s\n\tAtttribute : %s\n\tValue: %s\n",
-				"FMComms1", attrib, value);
-			return "FAIL";
-		}
-	}
-
-	return NULL;
+	snprintf(buf, sizeof(buf), "dds_mode = %i\n"
+			"dac_buf_filename = %s\n",
+			gtk_combo_box_get_active(GTK_COMBO_BOX(dds_mode)),
+			dac_buf_filename ?: "");
+	fwrite(buf, 1, strlen(buf), f);
 }
 
 static void context_destroy(const char *ini_fn)
@@ -2509,6 +2518,7 @@ static void context_destroy(const char *ini_fn)
 		if (vga)
 			save_to_ini(f, NULL, vga, fmcomms1_sr_attribs,
 					ARRAY_SIZE(fmcomms1_sr_attribs));
+		save_widgets_to_ini(f);
 		fclose(f);
 	}
 
@@ -2527,7 +2537,5 @@ struct osc_plugin plugin = {
 	.name = THIS_DRIVER,
 	.identify = fmcomms1_identify,
 	.init = fmcomms1_init,
-	.save_restore_attribs = fmcomms1_sr_attribs,
-	.handle_item = handle_item,
 	.destroy = context_destroy,
 };
