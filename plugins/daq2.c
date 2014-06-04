2d4b04ce6eb1ad520ac69546278e787e5be17960
Paul Cercueil <paul.cercueil@analog.com>
2014-06-04 11:42:15 +0200

    plugins/daq2: Update to use new INI functions
    Signed-off-by: Paul Cercueil <paul.cercueil@analog.com>

--- plugins/daq2.c
+++ plugins/daq2.c
@@ -24,12 +24,17 @@
 
 #include <iio.h>
 
+#include "../libini2.h"
 #include "../osc.h"
 #include "../iio_widget.h"
 #include "../osc_plugin.h"
 #include "../config.h"
 #include "../eeprom.h"
 #include "../ini/ini.h"
+
+#define THIS_DRIVER "DAQ2"
+
+#define ARRAY_SIZE(x) (!sizeof(x) ?: sizeof(x) / sizeof((x)[0]))
 
 static const gdouble mhz_scale = 1000000.0;
 static const gdouble khz_scale = 1000.0;
@@ -85,6 +90,27 @@ static GtkWidget *I_adc_offset_adj, *I_adc_gain_adj, *I_adc_phase_adj;
 static GtkWidget *Q_adc_offset_adj, *Q_adc_gain_adj, *Q_adc_phase_adj;
 
 static GtkWidget *ad9122_temp;
+
+static const char *daq2_sr_attribs[] = {
+	"axi-ad9680-hpc.in_voltage_sampling_frequency",
+	"axi-ad9144-hpc.out_altvoltage_sampling_frequency",
+	"dds_mode",
+	"dac_buf_filename",
+	"axi-ad9144-hpc.out_altvoltage0_1A_frequency",
+	"axi-ad9144-hpc.out_altvoltage2_2A_frequency",
+	"axi-ad9144-hpc.out_altvoltage1_1B_frequency",
+	"axi-ad9144-hpc.out_altvoltage3_2B_frequency",
+	"axi-ad9144-hpc.out_altvoltage0_1A_scale",
+	"axi-ad9144-hpc.out_altvoltage2_2A_scale",
+	"axi-ad9144-hpc.out_altvoltage1_1B_scale",
+	"axi-ad9144-hpc.out_altvoltage3_2B_scale",
+	"axi-ad9144-hpc.out_altvoltage0_1A_phase",
+	"axi-ad9144-hpc.out_altvoltage1_1B_phase",
+	"axi-ad9144-hpc.out_altvoltage2_2A_phase",
+	"axi-ad9144-hpc.out_altvoltage3_2B_phase",
+	SYNC_RELOAD,
+	NULL,
+};
 
 //static unsigned short temp_calibbias;
 
@@ -906,7 +932,7 @@ static void make_widget_update_signal_based(struct iio_widget *widgets,
 	}
 }
 
-static GtkWidget * daq2_init(GtkWidget *notebook)
+static GtkWidget * daq2_init(GtkWidget *notebook, const char *ini_fn)
 {
 	GtkBuilder *builder;
 	GtkWidget *daq2_panel;
@@ -918,6 +944,13 @@ static GtkWidget * daq2_init(GtkWidget *notebook)
 
 	dac = iio_context_find_device(ctx, "axi-ad9144-hpc");
 	adc = iio_context_find_device(ctx, "axi-ad9680-hpc");
+
+	if (ini_fn) {
+		update_from_ini(ini_fn, THIS_DRIVER, dac, daq2_sr_attribs,
+				ARRAY_SIZE(daq2_sr_attribs));
+		update_from_ini(ini_fn, THIS_DRIVER, adc, daq2_sr_attribs,
+				ARRAY_SIZE(daq2_sr_attribs));
+	}
 
 	builder = gtk_builder_new();
 
@@ -1111,8 +1144,6 @@ static GtkWidget * daq2_init(GtkWidget *notebook)
 	return daq2_panel;
 }
 
-#define SYNC_RELOAD "SYNC_RELOAD"
-
 static char *handle_item(struct osc_plugin *plugin, const char *attrib,
 			 const char *value)
 {
@@ -1152,31 +1183,19 @@ static char *handle_item(struct osc_plugin *plugin, const char *attrib,
 	return NULL;
 }
 
-static void context_destroy(void)
+static void context_destroy(const char *ini_fn)
 {
+	FILE *f = fopen(ini_fn, "a");
+	if (f) {
+		/* Write the section header */
+		save_to_ini(f, THIS_DRIVER, dac, daq2_sr_attribs,
+				ARRAY_SIZE(daq2_sr_attribs));
+		save_to_ini(f, NULL, adc, daq2_sr_attribs,
+				ARRAY_SIZE(daq2_sr_attribs));
+		fclose(f);
+	}
 	iio_context_destroy(ctx);
 }
-
-static const char *daq2_sr_attribs[] = {
-	"axi-ad9680-hpc.in_voltage_sampling_frequency",
-	"axi-ad9144-hpc.out_altvoltage_sampling_frequency",
-	"dds_mode",
-	"dac_buf_filename",
-	"axi-ad9144-hpc.out_altvoltage0_1A_frequency",
-	"axi-ad9144-hpc.out_altvoltage2_2A_frequency",
-	"axi-ad9144-hpc.out_altvoltage1_1B_frequency",
-	"axi-ad9144-hpc.out_altvoltage3_2B_frequency",
-	"axi-ad9144-hpc.out_altvoltage0_1A_scale",
-	"axi-ad9144-hpc.out_altvoltage2_2A_scale",
-	"axi-ad9144-hpc.out_altvoltage1_1B_scale",
-	"axi-ad9144-hpc.out_altvoltage3_2B_scale",
-	"axi-ad9144-hpc.out_altvoltage0_1A_phase",
-	"axi-ad9144-hpc.out_altvoltage1_1B_phase",
-	"axi-ad9144-hpc.out_altvoltage2_2A_phase",
-	"axi-ad9144-hpc.out_altvoltage3_2B_phase",
-	SYNC_RELOAD,
-	NULL,
-};
 
 static bool daq2_identify(void)
 {
@@ -1187,7 +1206,7 @@ static bool daq2_identify(void)
 }
 
 struct osc_plugin plugin = {
-	.name = "DAQ2",
+	.name = THIS_DRIVER,
 	.identify = daq2_identify,
 	.init = daq2_init,
 	.save_restore_attribs = daq2_sr_attribs,
