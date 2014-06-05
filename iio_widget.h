6b3d4886217af6d78330182868d47658731de645
Paul Cercueil <paul.cercueil@analog.com>
2014-06-05 16:01:30 +0200

    iio_widget: Add util function iio_update_widgets_of_device
    This function can be used by the plugins to update all the widgets
    related to one device. This function is optimized to speed up the
    update of the widgets when the network backend of libiio is used.
    
    Signed-off-by: Paul Cercueil <paul.cercueil@analog.com>

--- iio_widget.h
+++ iio_widget.h
@@ -23,6 +23,7 @@ struct iio_widget {
 
 	void (*save)(struct iio_widget *);
 	void (*update)(struct iio_widget *);
+	void (*update_value)(struct iio_widget *, const char *, size_t);
 };
 
 void g_builder_connect_signal(GtkBuilder *builder, const gchar *name,
@@ -34,6 +35,8 @@ void g_builder_bind_property(GtkBuilder *builder,
 
 void iio_update_widgets(struct iio_widget *widgets, unsigned int num_widgets);
 void iio_widget_update(struct iio_widget *widget);
+void iio_update_widgets_of_device(struct iio_widget *widgets,
+		unsigned int num_widgets, struct iio_device *dev);
 void iio_widget_save(struct iio_widget *widget);
 void iio_save_widgets(struct iio_widget *widgets, unsigned int num_widgets);
 
