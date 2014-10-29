9ce52307a76493a98f974887faa7f64b42802629
Paul Cercueil <paul.cercueil@analog.com>
2014-10-29 15:27:57 +0100

    plugins/fmcomms6: Convert to use new libini2 code
    Signed-off-by: Paul Cercueil <paul.cercueil@analog.com>

--- plugins/fmcomms6.c
+++ plugins/fmcomms6.c
@@ -28,7 +28,11 @@
 #include "../iio_widget.h"
 #include "../osc_plugin.h"
 #include "../config.h"
+#include "../libini2.h"
 
+#define THIS_DRIVER "FMComms6"
+
+#define ARRAY_SIZE(x) (!sizeof(x) ?: sizeof(x) / sizeof((x)[0]))
 
 #define ADC_DEVICE "axi-ad9652-lpc"
 #define PLL_DEVICE "adf4351-rx-lpc"
@@ -48,9 +52,20 @@ static struct iio_device *adc, *pll;
 static unsigned int rx_lo;
 
 static gint this_page;
-static GtkNotebook *nbook;
 static GtkWidget *fmcomms6_panel;
 static gboolean plugin_detached;
+
+static const char *fmcomms6_sr_attribs[] = {
+	ADC_DEVICE".in_voltage0_calibbias",
+	ADC_DEVICE".in_voltage1_calibbias",
+	ADC_DEVICE".in_voltage0_calibscale",
+	ADC_DEVICE".in_voltage1_calibscale",
+	ADC_DEVICE".in_voltage0_calibphase",
+	ADC_DEVICE".in_voltage1_calibphase",
+	PLL_DEVICE".out_altvoltage0_frequency_resolution",
+	PLL_DEVICE".out_altvoltage0_frequency",
+	PLL_DEVICE".out_altvoltage0_powerdown",
+};
 
 static void rx_update_values(void)
 {
@@ -106,12 +121,26 @@ static void reload_button_clicked(GtkButton *btn, gpointer data)
 	cal_update_values();
 }
 
-static int fmcomms6_init(GtkWidget *notebook)
+static void load_profile(const char *ini_fn)
+{
+	update_from_ini(ini_fn, THIS_DRIVER, adc, fmcomms6_sr_attribs,
+			ARRAY_SIZE(fmcomms6_sr_attribs));
+	update_from_ini(ini_fn, THIS_DRIVER, pll, fmcomms6_sr_attribs,
+			ARRAY_SIZE(fmcomms6_sr_attribs));
+}
+
+static GtkWidget * fmcomms6_init(GtkWidget *notebook, const char *ini_fn)
 {
 	GtkBuilder *builder;
 	struct iio_channel *ch0, *ch1;
 	builder = gtk_builder_new();
-	nbook = GTK_NOTEBOOK(notebook);
+
+	ctx = osc_create_context();
+	adc = iio_context_find_device(ctx, ADC_DEVICE);
+	pll = iio_context_find_device(ctx, PLL_DEVICE);
+
+	if (ini_fn)
+		load_profile(ini_fn);
 
 	if (!gtk_builder_add_from_file(builder, "fmcomms6.glade", NULL))
 		gtk_builder_add_from_file(builder, OSC_GLADE_FILE_PATH "fmcomms6.glade", NULL);
@@ -161,48 +190,8 @@ static int fmcomms6_init(GtkWidget *notebook)
 	rx_update_values();
 	cal_update_values();
 
-	this_page = gtk_notebook_append_page(nbook, fmcomms6_panel, NULL);
-	gtk_notebook_set_tab_label_text(nbook, fmcomms6_panel, "FMComms6");
-
-	return 0;
+	return fmcomms6_panel;
 }
-
-#define SYNC_RELOAD "SYNC_RELOAD"
-
-static char *handle_item(struct osc_plugin *plugin, const char *attrib,
-			 const char *value)
-{
-	if (MATCH_ATTRIB(SYNC_RELOAD)) {
-		if (value)
-			reload_button_clicked(NULL, 0);
-		else
-			return "1";
-	} else {
-		if (value) {
-			printf("Unhandled tokens in ini file,\n"
-				"\tSection %s\n\tAtttribute : %s\n\tValue: %s\n",
-				"FMComms6", attrib, value);
-			return "FAIL";
-		}
-	}
-
-	return NULL;
-}
-
-static const char *fmcomms6_sr_attribs[] = {
-	ADC_DEVICE".in_voltage0_calibbias",
-	ADC_DEVICE".in_voltage1_calibbias",
-	ADC_DEVICE".in_voltage0_calibscale",
-	ADC_DEVICE".in_voltage1_calibscale",
-	ADC_DEVICE".in_voltage0_calibphase",
-	ADC_DEVICE".in_voltage1_calibphase",
-	PLL_DEVICE".out_altvoltage0_frequency_resolution",
-	PLL_DEVICE".out_altvoltage0_frequency",
-	PLL_DEVICE".out_altvoltage0_powerdown",
-
-	SYNC_RELOAD,
-	NULL,
-};
 
 static void update_active_page(gint active_page, gboolean is_detached)
 {
@@ -218,38 +207,41 @@ static void fmcomms6_get_preferred_size(int *width, int *height)
 		*height = 480;
 }
 
-static void context_destroy(void)
+static void save_profile(const char *ini_fn)
 {
-	iio_context_destroy(ctx);
+	FILE *f = fopen(ini_fn, "a");
+	if (f) {
+		save_to_ini(f, THIS_DRIVER, adc, fmcomms6_sr_attribs,
+				ARRAY_SIZE(fmcomms6_sr_attribs));
+		save_to_ini(f, NULL, pll, fmcomms6_sr_attribs,
+				ARRAY_SIZE(fmcomms6_sr_attribs));
+		fclose(f);
+	}
 }
 
-struct osc_plugin plugin;
+static void context_destroy(const char *ini_fn)
+{
+	save_profile(ini_fn);
+	iio_context_destroy(ctx);
+}
 
 static bool fmcomms6_identify(void)
 {
 	/* Use the OSC's IIO context just to detect the devices */
 	struct iio_context *osc_ctx = get_context_from_osc();
 
-	if (!iio_context_find_device(osc_ctx, ADC_DEVICE)
-		|| !iio_context_find_device(osc_ctx, PLL_DEVICE))
-		return false;
 
-	ctx = osc_create_context();
-	adc = iio_context_find_device(ctx, ADC_DEVICE);
-	pll = iio_context_find_device(ctx, PLL_DEVICE);
-
-	if (!adc || !pll)
-		iio_context_destroy(ctx);
-	return !!adc && !!pll;
+	return !!iio_context_find_device(osc_ctx, ADC_DEVICE) &&
+		!!iio_context_find_device(osc_ctx, PLL_DEVICE);
 }
 
 struct osc_plugin plugin = {
-	.name = "FMComms6",
+	.name = THIS_DRIVER,
 	.identify = fmcomms6_identify,
 	.init = fmcomms6_init,
-	.save_restore_attribs = fmcomms6_sr_attribs,
-	.handle_item = handle_item,
 	.update_active_page = update_active_page,
 	.get_preferred_size = fmcomms6_get_preferred_size,
+	.save_profile = save_profile,
+	.load_profile = load_profile,
 	.destroy = context_destroy,
 };
