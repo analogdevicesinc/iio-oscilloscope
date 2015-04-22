analogdevicesinc/iio-oscilloscope

commit 449b589e7d12c0cf77749b2eece507f50bf2df5c

oscmain.c: Do not attempt to run a terminated program Check if the default profile hasn't already freed all resources as a result of parsing the "quit=1" line of the .ini file.
Signed-off-by: Dan Nechita <dan.nechita@analog.com>

oscmain.c
@@ -177,11 +177,13 @@ gint main (int argc, char **argv)
 	gdk_threads_enter();
 	init_application(profile);
 	c = load_default_profile(profile, false);
-	create_default_plot();
-	if (c == 0)
-		gtk_main();
-	else
-		application_quit();
+	if (ctx) {
+		create_default_plot();
+		if (c == 0)
+			gtk_main();
+		else
+			application_quit();
+	}
 	gdk_threads_leave();
 
 	if (profile)

commit 56ff7125d59e324816ff98d2aad7d5b84f2f267f

osc: Fix app not starting when an iio context cannot be created Bug was introduced by commit: 449b589e7d12c0cf77749b2eece507f50bf2df5c
Signed-off-by: Dan Nechita <dan.nechita@analog.com>

osc.c
@@ -51,6 +51,7 @@ GtkWidget *main_window;
 
 struct iio_context *ctx;
 unsigned int num_devices = 0;
+bool ctx_destroyed_by_do_quit;
 
 static void gfunc_save_plot_data_to_ini(gpointer data, gpointer user_data);
 static void plugin_restore_ini_state(const char *plugin_name,
@@ -1437,6 +1438,7 @@ static void do_quit(bool reload)
 	if (ctx) {
 		iio_context_destroy(ctx);
 		ctx = NULL;
+		ctx_destroyed_by_do_quit = true;
 	}
 
 	math_expression_objects_clean();

oscmain.c
@@ -13,6 +13,7 @@ extern GtkWidget *infobar;
 extern GtkWidget *tooltips_en;
 extern GtkWidget *main_window;
 extern struct iio_context *ctx;
+extern bool ctx_destroyed_by_do_quit;
 
 static void infobar_hide_cb(GtkButton *btn, gpointer user_data)
 {
@@ -165,7 +166,7 @@ gint main (int argc, char **argv)
 	gdk_threads_enter();
 	init_application();
 	c = load_default_profile(profile, true);
-	if (ctx) {
+	if (!ctx_destroyed_by_do_quit) {
 		create_default_plot();
 		if (c == 0)
 			gtk_main();