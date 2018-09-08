9a29dece8b49a97e5285b660b1cae867ab131039
Rodolfo García Peñas (kix) <kix@kix.es>
2018-09-08 14:57:11 +0200

    Avoid GCC 8 format-truncation error
    This patch avoids this problem:
    
    plugins/ad9371.c: In function ‘save_widgets_to_ini’:
    plugins/ad9371.c:1780:54: error: ‘%s’ directive output may be truncated writing up to 4095 bytes into a region of size 4072 [-Werror=format-truncation=]
      snprintf(buf, sizeof(buf), "load_myk_profile_file = %s\n"
                                                          ^~
    plugins/ad9371.c:1793:4:
        last_profile,
        ~~~~~~~~~~~~
    plugins/ad9371.c:1780:29: note: directive argument in the range [0, 1]
      snprintf(buf, sizeof(buf), "load_myk_profile_file = %s\n"
                                 ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    plugins/ad9371.c:1780:29: note: directive argument in the range [0, 1]
    plugins/ad9371.c:1780:29: note: directive argument in the range [0, 1]
    plugins/ad9371.c:1780:29: note: directive argument in the range [0, 1]
    plugins/ad9371.c:1780:29: note: directive argument in the range [0, 1]
    plugins/ad9371.c:1780:29: note: directive argument in the range [0, 1]
    plugins/ad9371.c:1780:29: note: directive argument in the range [0, 1]
    plugins/ad9371.c:1780:29: note: directive argument in the range [0, 1]
    plugins/ad9371.c:1780:29: note: directive argument in the range [0, 1]
    plugins/ad9371.c:1780:2: note: ‘snprintf’ output 224 or more bytes (assuming 4319) into a destination of size 4096
      snprintf(buf, sizeof(buf), "load_myk_profile_file = %s\n"
      ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
        "dds_mode_tx1 = %i\n"
        ~~~~~~~~~~~~~~~~~~~~~
        "dds_mode_tx2 = %i\n"
        ~~~~~~~~~~~~~~~~~~~~~
        "dac_buf_filename = %s\n"
        ~~~~~~~~~~~~~~~~~~~~~~~~~
        "tx_channel_0 = %i\n"
        ~~~~~~~~~~~~~~~~~~~~~
        "tx_channel_1 = %i\n"
        ~~~~~~~~~~~~~~~~~~~~~
        "tx_channel_2 = %i\n"
        ~~~~~~~~~~~~~~~~~~~~~
        "tx_channel_3 = %i\n"
        ~~~~~~~~~~~~~~~~~~~~~
        "global_settings_show = %i\n"
        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
        "tx_show = %i\n"
        ~~~~~~~~~~~~~~~~
        "rx_show = %i\n"
        ~~~~~~~~~~~~~~~~
        "obs_show = %i\n"
        ~~~~~~~~~~~~~~~~~
        "fpga_show = %i\n",
        ~~~~~~~~~~~~~~~~~~~
        last_profile,
        ~~~~~~~~~~~~~
        dac_data_manager_get_dds_mode(dac_tx_manager, DDS_DEVICE, 1),
        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
        dac_data_manager_get_dds_mode(dac_tx_manager, DDS_DEVICE, 2),
        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
        dac_data_manager_get_buffer_chooser_filename(dac_tx_manager),
        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
        dac_data_manager_get_tx_channel_state(dac_tx_manager, 0),
        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
        dac_data_manager_get_tx_channel_state(dac_tx_manager, 1),
        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
        dac_data_manager_get_tx_channel_state(dac_tx_manager, 2),
        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
        dac_data_manager_get_tx_channel_state(dac_tx_manager, 3),
        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
        !!gtk_toggle_tool_button_get_active(section_toggle[SECTION_GLOBAL]),
        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
        !!gtk_toggle_tool_button_get_active(section_toggle[SECTION_TX]),
        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
        !!gtk_toggle_tool_button_get_active(section_toggle[SECTION_RX]),
        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
        !!gtk_toggle_tool_button_get_active(section_toggle[SECTION_OBS]),
        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
        !!gtk_toggle_tool_button_get_active(section_toggle[SECTION_FPGA]));
        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    cc1: all warnings being treated as errors
    make: *** [Makefile:150: plugins/ad9371.so] Error 1

--- plugins/ad9371.c
+++ plugins/ad9371.c
@@ -1777,7 +1777,7 @@ static void save_widgets_to_ini(FILE *f)
 {
 	char buf[0x1000];
 
-	snprintf(buf, sizeof(buf), "load_myk_profile_file = %s\n"
+	if (snprintf(buf, sizeof(buf), "load_myk_profile_file = %s\n"
 			"dds_mode_tx1 = %i\n"
 			"dds_mode_tx2 = %i\n"
 			"dac_buf_filename = %s\n"
@@ -1802,7 +1802,9 @@ static void save_widgets_to_ini(FILE *f)
 			!!gtk_toggle_tool_button_get_active(section_toggle[SECTION_TX]),
 			!!gtk_toggle_tool_button_get_active(section_toggle[SECTION_RX]),
 			!!gtk_toggle_tool_button_get_active(section_toggle[SECTION_OBS]),
-			!!gtk_toggle_tool_button_get_active(section_toggle[SECTION_FPGA]));
+			!!gtk_toggle_tool_button_get_active(section_toggle[SECTION_FPGA])) == sizeof(buf))
+		return;
+
 	fwrite(buf, 1, strlen(buf), f);
 }
 
