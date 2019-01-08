95f1c34721d91a03b4076133f6d61c94a8836e57
Alexandru Ardelean <alexandru.ardelean@analog.com>
2019-01-08 11:48:35 +0200

    CI/travis/lib.sh: overwrite file with version libad9361-iio
    The version in the `libad9361-iio` does a good job at downloading the
    `lib.sh` from the libiio repo, which contains more build code.
    
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>

--- CI/travis/lib.sh
+++ CI/travis/lib.sh
@@ -1,10 +1,57 @@
-#!/bin/bash
+#!/bin/sh -xe
 
-export NUM_JOBS=4
-export WORKDIR="${PWD}/deps"
+TRAVIS_BUILD_DIR=${TRAVIS_BUILD_DIR:-'./'}
+
+WORKDIR="${PWD}/deps"
 mkdir -p "$WORKDIR"
-if [ "$TRAVIS" == "true" ] ; then
-	export STAGINGDIR=/usr/local
+if [ "$TRAVIS" = "true" ] || [ "$INSIDE_DOCKER" = "1" ] ; then
+	STAGINGDIR=/usr
 else
-	export STAGINGDIR="${WORKDIR}/staging"
+	STAGINGDIR="${WORKDIR}/staging"
+fi
+
+LIBIIO_BRANCH=master
+
+command_exists() {
+	local cmd=$1
+	[ -n "$cmd" ] || return 1
+	type "$cmd" >/dev/null 2>&1
+}
+
+ensure_command_exists() {
+	local cmd="$1"
+	local package="$2"
+	[ -n "$cmd" ] || return 1
+	[ -n "$package" ] || package="$cmd"
+	! command_exists "$cmd" || return 0
+	# go through known package managers
+	for pacman in apt-get brew yum ; do
+		command_exists $pacman || continue
+		$pacman install -y $package || {
+			# Try an update if install doesn't work the first time
+			$pacman -y update && \
+				$pacman install -y $package
+		}
+		return $?
+	done
+	return 1
+}
+
+ensure_command_exists wget
+ensure_command_exists sudo
+
+# Get the common stuff from libiio
+[ -f ${TRAVIS_BUILD_DIR}/build/lib.sh ] || {
+	mkdir -p ${TRAVIS_BUILD_DIR}/build
+	wget https://raw.githubusercontent.com/analogdevicesinc/libiio/master/CI/travis/lib.sh \
+		-O ${TRAVIS_BUILD_DIR}/build/lib.sh
+}
+
+. ${TRAVIS_BUILD_DIR}/build/lib.sh
+
+if [ -z "${LDIST}" -a -f "build/.LDIST" ] ; then
+	export LDIST="-$(cat build/.LDIST)"
+fi
+if [ -z "${LDIST}" ] || [ "$LDIST" = "DO_NOT_DEPLOY" ] ; then
+	export LDIST="-$(get_ldist)"
 fi
