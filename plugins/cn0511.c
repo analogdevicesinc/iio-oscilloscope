d68f19c5180b8a39b5ed4b486d8633c014ae8153
Nuno Sá <nuno.sa@analog.com>
2019-07-17 13:07:39 +0200

    plugins: cn0511: Support cn0511 cape
    This is the initial support for the cn0511 cape. This cape is a signal
    generator based on the ad9166 wideband transmiter and the adf4372 PLL.
    This plugins allows osc to manipulate this device.
    
    Signed-off-by: Nuno Sá <nuno.sa@analog.com>

---
+++ plugins/cn0511.c
@@ -0,0 +1,206 @@
+/**
+ * Copyright (C) 2019 Analog Devices, Inc.
+ *
+ * Licensed under the GPL-2.
+ *
+ **/
+#include <errno.h>
+#include <gtk/gtk.h>
+#include <glib.h>
+#include <math.h>
+
+#include "../osc.h"
+#include "../osc_plugin.h"
+#include "../iio_widget.h"
+
+#define THIS_DRIVER	"CN0511"
+#define DAC_DEVICE	"ad9166"
+#define DAC_AMPLIFIER	"ad9166-amp"
+/*
+ * For now leave this define as it is but, in future it would be nice if
+ * the scaling is done in the driver so that, the plug in does not have
+ * to knwow about  DAC_MAX_AMPLITUDE.
+ */
+#define DAC_MAX_AMPLITUDE	32767.0
+#define SCALE_MINUS_INFINITE	-91.0
+#define SCALE_MAX		0
+
+static struct iio_context *ctx;
+
+static struct iio_widget iio_widgets[25];
+static unsigned int num_widgets;
+
+static GtkWidget *cn0511_panel;
+static GtkWidget *scale_offset;
+static GtkAdjustment *adj_scale;
+
+static gboolean plugin_detached;
+static gint this_page;
+static int scale_offset_db = 0;
+
+const gdouble mhz_scale = 1000000.0;
+
+static double db_full_scale_convert(double value, bool inverse)
+{
+	if (inverse) {
+		if (value == 0)
+			return -DBL_MAX;
+
+		return (int)((20 * log10(value / DAC_MAX_AMPLITUDE)) -
+			     scale_offset_db);
+	}
+
+	return DAC_MAX_AMPLITUDE * pow(10, (value + scale_offset_db) / 20.0);
+}
+
+static void save_scale_offset(GtkSpinButton *btn, gpointer data)
+{
+	scale_offset_db = gtk_spin_button_get_value(btn);
+	gtk_adjustment_set_upper(adj_scale, SCALE_MAX - scale_offset_db);
+	gtk_adjustment_set_lower(adj_scale,
+				 SCALE_MINUS_INFINITE - scale_offset_db);
+}
+
+static void save_widget_value(GtkWidget *widget, struct iio_widget *iio_w)
+{
+	iio_w->save(iio_w);
+	/* refresh widgets so that, we know if our value was updated */
+	iio_update_widgets(iio_widgets, num_widgets);
+}
+
+static void make_widget_update_signal_based(struct iio_widget *widgets,
+		unsigned int num_widgets)
+{
+	char signal_name[25];
+	unsigned int i;
+
+	for (i = 0; i < num_widgets; i++) {
+		if (GTK_IS_CHECK_BUTTON(widgets[i].widget))
+			sprintf(signal_name, "%s", "toggled");
+		else if (GTK_IS_TOGGLE_BUTTON(widgets[i].widget))
+			sprintf(signal_name, "%s", "toggled");
+		else if (GTK_IS_SPIN_BUTTON(widgets[i].widget))
+			sprintf(signal_name, "%s", "value-changed");
+		else if (GTK_IS_COMBO_BOX_TEXT(widgets[i].widget))
+			sprintf(signal_name, "%s", "changed");
+		else
+			printf("unhandled widget type, attribute: %s\n",
+			       widgets[i].attr_name);
+
+		if (GTK_IS_SPIN_BUTTON(widgets[i].widget) &&
+		    widgets[i].priv_progress != NULL) {
+			iio_spin_button_progress_activate(&widgets[i]);
+		} else {
+			g_signal_connect(G_OBJECT(widgets[i].widget),
+					 signal_name,
+					 G_CALLBACK(save_widget_value),
+					 &widgets[i]);
+		}
+	}
+}
+
+static GtkWidget *cn0511_init(struct osc_plugin *plugin, GtkWidget *notebook,
+			      const char *ini_fn)
+{
+	GtkBuilder *builder;
+	struct iio_channel *dac_ch;
+	struct iio_device *dac;
+	struct iio_device *dac_amp;
+
+	builder = gtk_builder_new();
+
+	ctx = osc_create_context();
+	if (!ctx)
+		return NULL;
+
+	if (osc_load_glade_file(builder, "cn0511") < 0)
+		return NULL;
+
+	dac = iio_context_find_device(ctx, DAC_DEVICE);
+	dac_amp = iio_context_find_device(ctx, DAC_AMPLIFIER);
+
+	if (!dac || !dac_amp) {
+		printf("Could not find expected iio devices\n");
+		return NULL;
+	}
+
+	cn0511_panel = GTK_WIDGET(gtk_builder_get_object(builder,
+							 "cn0511_panel"));
+	scale_offset = GTK_WIDGET(gtk_builder_get_object(builder,
+						"spinbutton_nco_offset"));
+	adj_scale = GTK_ADJUSTMENT(gtk_builder_get_object(builder,
+						"adj_altVoltage0_scale"));
+
+	dac_ch = iio_device_find_channel(dac, "altvoltage0", true);
+
+	iio_spin_button_int_init_from_builder(&iio_widgets[num_widgets++], dac,
+					      NULL, "sampling_frequency",
+					      builder, "spinbutton_sample_freq",
+					      &mhz_scale);
+
+	iio_spin_button_int_init_from_builder(&iio_widgets[num_widgets++], dac,
+					      dac_ch, "nco_frequency",
+					      builder, "spinbutton_nco_freq",
+					      &mhz_scale);
+
+	iio_spin_button_int_init_from_builder(&iio_widgets[num_widgets++], dac,
+					      dac_ch, "raw", builder,
+					      "spinbutton_nco_scale", NULL);
+	iio_spin_button_set_convert_function(&iio_widgets[num_widgets - 1],
+					     db_full_scale_convert);
+
+	iio_toggle_button_init_from_builder(&iio_widgets[num_widgets++],
+					    dac_amp, NULL, "en", builder,
+					    "dac_amplifier_enable", 0);
+
+
+	make_widget_update_signal_based(iio_widgets, num_widgets);
+	iio_update_widgets(iio_widgets, num_widgets);
+
+	g_signal_connect(G_OBJECT(scale_offset), "value-changed",
+			 G_CALLBACK(save_scale_offset), NULL);
+
+	return cn0511_panel;
+}
+
+static void update_active_page(struct osc_plugin *plugin, gint active_page,
+			       gboolean is_detached)
+{
+	this_page = active_page;
+	plugin_detached = is_detached;
+}
+
+static void cn0511_get_preferred_size(const struct osc_plugin *plugin,
+				      int *width, int *height)
+{
+	if (width)
+		*width = 640;
+	if (height)
+		*height = 480;
+}
+
+static void context_destroy(struct osc_plugin *plugin, const char *ini_fn)
+{
+	g_source_remove_by_user_data(ctx);
+	osc_destroy_context(ctx);
+}
+
+struct osc_plugin plugin;
+
+static bool cn0511_identify(const struct osc_plugin *plugin)
+{
+	/* Use the OSC's IIO context just to detect the devices */
+	struct iio_context *osc_ctx = get_context_from_osc();
+
+	return !!iio_context_find_device(osc_ctx, DAC_DEVICE) &&
+		!!iio_context_find_device(osc_ctx, DAC_AMPLIFIER);
+}
+
+struct osc_plugin plugin = {
+	.name = THIS_DRIVER,
+	.identify = cn0511_identify,
+	.init = cn0511_init,
+	.update_active_page = update_active_page,
+	.get_preferred_size = cn0511_get_preferred_size,
+	.destroy = context_destroy,
+};
