17701390bf3f6d8b49904f462769636432c821c7
Dan Nechita <dan.nechita@analog.com>
2019-11-28 10:49:19 +0200

    iio_utils: Add helper that gets device channels sorted naturally
    Expose the method to compare string naturally to be used in other places as well.
    
    Signed-off-by: Dan Nechita <dan.nechita@analog.com>

--- iio_utils.h
+++ iio_utils.h
@@ -12,4 +12,8 @@
 
 GArray * get_iio_devices_starting_with(struct iio_context *ctx, const char *sequence);
 
+GArray * get_iio_channels_naturally_sorted(struct iio_device *dev);
+
+int str_natural_cmp(const char *s1, const char *s2);
+
 #endif  /* __IIO_UTILS__ */
