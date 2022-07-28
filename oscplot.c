96bbede9aaec30c268720d630a1bb59f568f83a7
Dan Nechita <dan.nechita@analog.com>
2022-07-28 13:53:44 +0300

    oscplot: Don't free the CssProvider
    It seems that the GtkCssProvider doesn't need to be freed manually. Most likely is being freed by the screen to which the provider is attached. With this change, osc no longer crashes when exiting the application.
    
    Signed-off-by: Dan Nechita <dan.nechita@analog.com>

--- oscplot.c
+++ oscplot.c
@@ -486,7 +486,6 @@ GtkWidget *osc_plot_new_with_pref(struct iio_context *ctx, OscPlotPreferences *p
 
 void osc_plot_destroy (OscPlot *plot)
 {	
-	g_free(G_OBJECT(plot->priv->provider));
 	gtk_widget_destroy(plot->priv->window);
 	gtk_widget_destroy(GTK_WIDGET(plot));
 }
