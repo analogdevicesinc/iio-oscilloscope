6789e3f54be1495a149e25323b471d71d0969264
Cristina Suteu <cristina.suteu@analog.com>
2022-11-14 14:24:42 +0200

    add docker for windows build
    added docker file with msys2 to build the application on native,
    instead of cross compiling
    
    added script that installs/builds dependencies and osc application on msys2
    
    added script that creates the required directory structure for the installer
    and copies the dependencies to the correct location
    
    
    Signed-off-by: Cristina Suteu <cristina.suteu@analog.com>

---
+++ docker/build_mingw.sh
@@ -0,0 +1,126 @@
+#/bin/bash
+set -xe
+LIBIIO_BRANCH=master
+LIBAD9361_BRANCH=master
+LIBAD9166_BRANCH=master
+
+export WORKDIR=/home/$USER/
+
+init_env() {
+
+export ARCH=x86_64
+export MINGW_VERSION=mingw64
+export CC=/${MINGW_VERSION}/bin/${ARCH}-w64-mingw32-gcc.exe
+export CXX=/${MINGW_VERSION}/bin/${ARCH}-w64-mingw32-g++.exe
+
+export JOBS=-j9
+export STAGING_DIR="/mingw64"
+export MAKE_BIN="/bin/make"
+export MAKE="$MAKE_BIN $JOBS"
+export CMAKE_OPTS="-DCMAKE_C_COMPILER:FILEPATH=${CC} \
+-DCMAKE_CXX_COMPILER:FILEPATH=${CXX} \
+-DCMAKE_MAKE_PROGRAM:FILEPATH=${MAKE_BIN} \
+-DPKG_CONFIG_EXECUTABLE=/$MINGW_VERSION/bin/pkg-config.exe \
+-DCMAKE_MODULE_PATH=$STAGING_DIR \
+-DCMAKE_PREFIX_PATH=$STAGING_DIR/lib/cmake \
+-DCMAKE_BUILD_TYPE=RelWithDebInfo \
+-DCMAKE_STAGING_PREFIX=$STAGING_DIR \
+-DCMAKE_INSTALL_PREFIX=$STAGING_DIR"
+
+export CMAKE="/$MINGW_VERSION/bin/cmake"
+export HOST=${MINGW_VERSION}-w64-mingw32
+export AUTOCONF_OPTS="--prefix=/mingw64 \
+        --host=${ARCH}-w64-mingw32 \
+        --enable-shared \
+        --disable-static"
+}
+
+install_pacman_deps() {
+WINDEPS="mingw-w64-x86_64-gdb \
+mingw-w64-x86_64-dlfcn \
+mingw-w64-x86_64-libserialport \
+mingw-w64-x86_64-libusb \
+mingw-w64-x86_64-matio \
+mingw-w64-x86_64-fftw \
+mingw-w64-x86_64-autotools \
+mingw-w64-x86_64-gtk3 \
+mingw-w64-x86_64-glib2 \
+mingw-w64-x86_64-cmake \
+mingw-w64-x86_64-gcc \
+mingw-w64-x86_64-python3 \
+mingw-w64-x86_64-autotools \
+mingw-w64-x86_64-make \
+mingw-w64-x86_64-doxygen \
+mingw-w64-x86_64-jansson \
+cmake \
+vim \
+git \
+"
+pacman -S --noconfirm --needed $WINDEPS
+}
+
+
+build_libiio() {
+    pushd $WORKDIR
+    git clone https://github.com/analogdevicesinc/libiio --branch $LIBIIO_BRANCH
+    cd libiio
+    rm -rf build
+    mkdir build 
+    cd build
+    $CMAKE $CMAKE_OPTS -G"Unix Makefiles" -DWITH_SERIAL_BACKEND=ON ../
+    $MAKE install
+    popd
+}
+
+build_libad9361() {
+    pushd $WORKDIR
+    git clone https://github.com/analogdevicesinc/libad9361-iio --branch $LIBAD9361_BRANCH
+    cd libad9361-iio
+    mkdir build 
+    cd build
+    $CMAKE $CMAKE_OPTS -G"Unix Makefiles" ../
+    $MAKE install
+    popd
+}
+
+build_libad9166 () {
+    pushd $WORKDIR
+    git clone https://github.com/analogdevicesinc/libad9166-iio --branch $LIBAD9166_BRANCH
+    cd libad9166-iio
+    mkdir build 
+    cd build
+    $CMAKE $CMAKE_OPTS -G"Unix Makefiles" ../
+    $MAKE install
+    popd
+}
+
+build_gtkdatabox () {
+    pushd $WORKDIR
+    wget https://downloads.sourceforge.net/project/gtkdatabox/gtkdatabox-1/gtkdatabox-1.0.0.tar.gz
+    tar xvf gtkdatabox-1.0.0.tar.gz
+    cd gtkdatabox-1.0.0
+    ./configure $AUTOCONF_OPTS
+    $MAKE install
+    popd
+}
+
+build_deps() {
+    build_libiio
+    build_libad9361
+    build_libad9166
+    build_gtkdatabox
+}
+
+build_osc() {
+    
+    pushd $WORKDIR/iio-oscilloscope
+    mkdir build
+    cd build
+    $CMAKE $CMAKE_OPTS -G"Unix Makefiles" ../
+    $MAKE 
+    popd
+}
+
+init_env
+$@
+
---
+++ docker/install_prep.sh
@@ -0,0 +1,120 @@
+#/bin/bash
+set -xe
+
+export WORKDIR=/home/$USER/
+export STAGING_DIR="/mingw64"
+export STAGING_BIN="$STAGING_DIR/bin"
+export DLLS="$STAGING_BIN/libad9166.dll \
+$STAGING_BIN/libad9361.dll \
+$STAGING_BIN/libatk-1.0-0.dll \
+$STAGING_BIN/libbrotlicommon.dll \
+$STAGING_BIN/libbrotlidec.dll \
+$STAGING_BIN/libbz2-1.dll \
+$STAGING_BIN/libcairo-2.dll \
+$STAGING_BIN/libcairo-gobject-2.dll \
+$STAGING_BIN/libcrypto-1_1-x64.dll \
+$STAGING_BIN/libcurl-4.dll \
+$STAGING_BIN/libdatrie-1.dll \
+$STAGING_BIN/libepoxy-0.dll \
+$STAGING_BIN/libexpat-1.dll \
+$STAGING_BIN/libffi-8.dll \
+$STAGING_BIN/libfftw3-3.dll \
+$STAGING_BIN/libfontconfig-1.dll \
+$STAGING_BIN/libfreetype-6.dll \
+$STAGING_BIN/libfribidi-0.dll \
+$STAGING_BIN/libgcc_s_seh-1.dll \
+$STAGING_BIN/libgdk_pixbuf-2.0-0.dll \
+$STAGING_BIN/libgdk-3-0.dll \
+$STAGING_BIN/libgio-2.0-0.dll \
+$STAGING_BIN/libglib-2.0-0.dll \
+$STAGING_BIN/libgmodule-2.0-0.dll \
+$STAGING_BIN/libgobject-2.0-0.dll \
+$STAGING_BIN/libgraphite2.dll \
+$STAGING_BIN/libgthread-2.0-0.dll \
+$STAGING_BIN/libgtk-3-0.dll \
+$STAGING_BIN/libgtkdatabox-1.dll \
+$STAGING_BIN/libharfbuzz-0.dll \
+$STAGING_BIN/libhdf5-0.dll \
+$STAGING_BIN/libiconv-2.dll \
+$STAGING_BIN/libidn2-0.dll \
+$STAGING_BIN/libiio.dll \
+$STAGING_BIN/libintl-8.dll \
+$STAGING_BIN/libjansson-4.dll \
+$STAGING_BIN/liblzma-5.dll \
+$STAGING_BIN/libmatio-11.dll \
+$STAGING_BIN/libnghttp2-14.dll \
+$STAGING_BIN/libpango-1.0-0.dll \
+$STAGING_BIN/libpangocairo-1.0-0.dll \
+$STAGING_BIN/libpangoft2-1.0-0.dll \
+$STAGING_BIN/libpangowin32-1.0-0.dll \
+$STAGING_BIN/libpcre2-8-0.dll \
+$STAGING_BIN/libpcre2-32-0.dll \
+$STAGING_BIN/libpixman-1-0.dll \
+$STAGING_BIN/libpng16-16.dll \
+$STAGING_BIN/libpsl-5.dll \
+$STAGING_BIN/librsvg-2-2.dll \
+$STAGING_BIN/libserialport-0.dll \
+$STAGING_BIN/libssh2-1.dll \
+$STAGING_BIN/libssl-1_1-x64.dll \
+$STAGING_BIN/libstdc++-6.dll \
+$STAGING_BIN/libsz.dll \
+$STAGING_BIN/libthai-0.dll \
+$STAGING_BIN/libunistring-2.dll \
+$STAGING_BIN/libusb-1.0.dll \
+$STAGING_BIN/libwinpthread-1.dll \
+$STAGING_BIN/libxml2-2.dll \
+$STAGING_BIN/libzstd.dll \
+$STAGING_BIN/zlib1.dll 
+"
+export EXES="$STAGING_BIN/curl.exe \
+$STAGING_BIN/iio_genxml.exe \
+$STAGING_BIN/iio_info.exe \
+$STAGING_BIN/iio_readdev.exe
+"
+
+bin_dir() {
+	pushd $WORKDIR
+	mkdir $WORKDIR/iio-oscilloscope/build/bin
+	cp  $WORKDIR/iio-oscilloscope/build/osc.exe $WORKDIR/iio-oscilloscope/build/bin
+	cp  $WORKDIR/iio-oscilloscope/build/styles.css $WORKDIR/iio-oscilloscope/build/bin
+	cp  $WORKDIR/iio-oscilloscope/build/libosc.dll $WORKDIR/iio-oscilloscope/build/bin
+	cp -r $WORKDIR/iio-oscilloscope/glade/ $WORKDIR/iio-oscilloscope/build/bin
+	cp $DLLS $WORKDIR/iio-oscilloscope/build/bin
+	cp $EXES $WORKDIR/iio-oscilloscope/build/bin
+	popd
+	
+
+}
+
+lib_dir() {
+	pushd $WORKDIR
+	mkdir $WORKDIR/iio-oscilloscope/build/lib
+	cp -r $STAGING_DIR/lib/gdk-pixbuf-2.0 $WORKDIR/iio-oscilloscope/build/lib
+	cp -r $STAGING_DIR/lib/gtk-3.0 $WORKDIR/iio-oscilloscope/build/lib
+	mkdir $WORKDIR/iio-oscilloscope/build/lib/osc
+	cp -r $WORKDIR/iio-oscilloscope/block_diagrams $WORKDIR/iio-oscilloscope/build/lib/osc
+	cp -r $WORKDIR/iio-oscilloscope/filters $WORKDIR/iio-oscilloscope/build/lib/osc
+	cp -r $WORKDIR/iio-oscilloscope/build/profiles $WORKDIR/iio-oscilloscope/build/lib/osc
+	cp -r $WORKDIR/iio-oscilloscope/waveforms $WORKDIR/iio-oscilloscope/build/lib/osc
+	cp -r $WORKDIR/iio-oscilloscope/xmls $WORKDIR/iio-oscilloscope/build/lib/osc
+	cp $WORKDIR/iio-oscilloscope/build/plugins/*.dll $WORKDIR/iio-oscilloscope/build/lib/osc
+	popd
+}
+
+share_dir() {
+	pushd $WORKDIR
+	mkdir $WORKDIR/iio-oscilloscope/build/share
+	cp -r $STAGING_DIR/share/locale $WORKDIR/iio-oscilloscope/build/share
+	cp -r $STAGING_DIR/share/themes $WORKDIR/iio-oscilloscope/build/share
+	mkdir $WORKDIR/iio-oscilloscope/build/share/icons
+	cp -r $STAGING_DIR/share/icons/Adwaita $WORKDIR/iio-oscilloscope/build/share/icons
+	cp -r $STAGING_DIR/share/icons/hicolor $WORKDIR/iio-oscilloscope/build/share/icons
+	mkdir $WORKDIR/iio-oscilloscope/build/share/glib-2.0
+	cp -r $STAGING_DIR/share/glib-2.0/schemas $WORKDIR/iio-oscilloscope/build/share/glib-2.0
+	popd
+
+}
+
+bin_dir
+lib_dir
+share_dir
