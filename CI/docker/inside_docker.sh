39c5308a063f6112073baeb2badb1bde0ec3880e
Cristina Suteu <cristina.suteu@analog.com>
2023-04-26 07:55:25 +0300

    CI/docker: build app with latest scripts move osc repo cloning to workflow remove git cloning from build script
    Signed-off-by: Cristina Suteu <cristina.suteu@analog.com>

--- CI/docker/inside_docker.sh
+++ CI/docker/inside_docker.sh
@@ -2,20 +2,22 @@
 set -xe
 
 export SRCDIR=/home/docker/iio-oscilloscope
+cd $SRCDIR
 
 # set osc version
 
-export OSC_BUILD_VER=`git describe --tags --always HEAD`
 sed "s/UNSET_VERSION/${OSC_BUILD_VER}/" osc.iss > updated-osc.iss
 
 # build osc
 
-./build_mingw.sh build_osc
+./CI/docker/build_mingw.sh build_osc
 
 # create dir hierarchy
 
-./install_prep.sh
+./CI/docker/install_prep.sh
 
 # run innosetup 
 
 /c/innosetup/iscc //p $SRCDIR/updated-osc.iss
+
+mv ./artifact/adi-osc-setup.exe /home/docker/artifact
