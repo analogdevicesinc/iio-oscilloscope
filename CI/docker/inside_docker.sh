2e6ce7d2e7ad63fe3888dad0f42a16883ce83917
Cristina Suteu <cristina.suteu@analog.com>
2023-05-03 15:23:30 +0300

    mingw-build: build osc with latest iio deps
    build latest libiio, libad9361, libad9661 inside windows docker
    pass branches for lib repos as environment variables
    
    
    Signed-off-by: Cristina Suteu <cristina.suteu@analog.com>

--- CI/docker/inside_docker.sh
+++ CI/docker/inside_docker.sh
@@ -8,6 +8,18 @@ cd $SRCDIR
 
 sed "s/UNSET_VERSION/${OSC_BUILD_VER}/" osc.iss > updated-osc.iss
 
+# build libiio
+
+./CI/docker/build_mingw.sh build_libiio
+
+# build libad9361
+
+./CI/docker/build_mingw.sh build_libad9361
+
+# build libad9166 
+
+./CI/docker/build_mingw.sh build_libad9166
+
 # build osc
 
 ./CI/docker/build_mingw.sh build_osc
