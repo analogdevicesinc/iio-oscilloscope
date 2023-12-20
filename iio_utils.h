16a03416341f1acd214ee72f3a512ac3af5d15af
Nuno Sa <nuno.sa@analog.com>
2023-12-20 10:15:56 +0100

    iio_utils: add helper API to see if attribute is not present
    Often, one wants to enable/disable certain features in a plugin
    depending on the present or not of some attribute. hence, let's add an
    helper for it. For now, this only supports checking device and channel
    attributes as that should cover most of the cases.
    
    Signed-off-by: Nuno Sa <nuno.sa@analog.com>

--- iio_utils.h
+++ iio_utils.h
@@ -24,5 +24,6 @@ int str_natural_cmp(const char *s1, const char *s2);
 void handle_toggle_section_cb(struct _GtkToggleToolButton *btn, struct _GtkWidget *section);
 
 const char *get_iio_device_label_or_name(const struct iio_device *dev);
+bool iio_attr_not_found(struct iio_device *dev, struct iio_channel *chn, const char *attr_name);
 
 #endif  /* __IIO_UTILS__ */
